612004da2b50167b6b5a7b2cf69e5bdbbca1ccb3
Hong Gu <macrogu@qq.com>
2021-12-28 17:59:49 +0800

    init empty project

---
+++ Source/MetaMMO/MetaMMO.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MetaMMO.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MetaMMO, "MetaMMO" );
---
+++ Source/MetaMMO/MetaMMOGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MetaMMOGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class METAMMO_API AMetaMMOGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
